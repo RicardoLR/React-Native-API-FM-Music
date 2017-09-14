b2293c10b28fcfd0ddfa258df650b41fc3549985
Ricardo <ricardolopez.computing@gmail.com>
2017-09-13 23:41:57 -0500

    Android - IOS SKD de FB

--- android/app/src/main/java/com/music/MainActivity.java
+++ android/app/src/main/java/com/music/MainActivity.java
@@ -1,6 +1,11 @@
 package com.music;
 
 import com.facebook.react.ReactActivity;
+    
+/** =============================================
+Lo agrago por mi cuenta para trabajar con SKD Facebook 
+============================================= */
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +17,13 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "Music";
     }
+
+    /** =============================================
+    Lo agrago por mi cuenta para trabajar con SKD Facebook 
+    ============================================= */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+       super.onActivityResult(requestCode, resultCode, data);
+       MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }    
 }
